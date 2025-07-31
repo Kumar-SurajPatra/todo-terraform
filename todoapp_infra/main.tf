b52e44329fc3b31cb4dac09d93ef2e61954dc69e
Suraj-q <surajkp369@gmail.com>
2025-08-01 00:11:19 +0530

    Revert "New RG and Public IP description added"

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -4,12 +4,6 @@ module "resource_group" {
   resource_group_location = "centralindia"
 }
 
-module "resource_group2" {
-  source                  = "../modules/azurerm_resource_group"
-  resource_group_name     = "rg-todoapp2"
-  resource_group_location = "centralindia"
-}
-
 module "vitual_network" {
   depends_on         = [module.resource_group]
   source             = "../modules/azurerm_virtual_network"
